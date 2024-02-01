b8a056f0a689b281c22ad6de7ac4c6027cbe72ea
Crmorale <152791634+Crmorale@users.noreply.github.com>
2024-02-01 22:20:12 +0100

    Add files via upload

---
+++ libft/functions/ft_bzero.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: crmorale <crmorale@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/10 13:57:37 by crmorale          #+#    #+#             */
+/*   Updated: 2023/12/10 17:59:04 by crmorale         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+void	ft_bzero(void *s, size_t n)
+{
+	char	*str;
+	size_t	i;
+
+	str = (char *)s;
+	i = 0;
+	
+	while (i < n)
+		{
+			str[i] = 0;
+			i++;
+		}
+}
+
+int main(void)
+{
+	char str1 []= "Habemus pescadus";
+	int len = 5;
+	char str2[sizeof(str1)];
+	
+	strcpy(str2, str1);	
+
+	printf("Antes de ft_bzero: %s", str2);
+	
+	ft_bzero(str2, len);
+	printf("%s", str2);
+
+	return (0);
+}
---
+++ libft/functions/ft_memset.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: crmorale <crmorale@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/05 21:31:23 by crmorale          #+#    #+#             */
+/*   Updated: 2023/12/10 14:54:25 by crmorale         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	char	*str;
+	size_t	i;
+
+	str = (char *)b;
+	i = 0;
+
+	while (i < len)
+	{
+		str[i] = c;
+		i++;
+	}
+	return (b);
+}
+
+int	main(void)
+{
+	char	str []= "Hola Jacinto";
+	int	letter;
+	size_t len;
+		
+	letter = 'A';
+	len = 5;
+	
+	printf("%s", ft_memset(str, letter, len));
+	return (0);
+}
